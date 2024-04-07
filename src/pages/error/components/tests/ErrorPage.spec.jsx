8872c514cd2231a002212e35c60cf580f5e7532c
SOPLAY <tkdgns1341@gmail.com>
2024-04-07 16:46:06 +0900

    Add : mock을 사용한 테스트 코드 작성

--- src/pages/error/components/tests/ErrorPage.spec.jsx
+++ src/pages/error/components/tests/ErrorPage.spec.jsx
@@ -4,4 +4,20 @@ import React from 'react';
 import ErrorPage from '@/pages/error/components/ErrorPage';
 import render from '@/utils/test/render';
 
-it('"뒤로 이동" 버튼 클릭시 뒤로 이동하는 navigate(-1) 함수가 호출된다', async () => {});
+// 실제 모듈을 모킹한 모듈로 대체하여 테스트 실행
+// useNavigate 훅으로 반환받은 navigate 함수가 올바르게 호출되었는가 -> 스파이 함수
+const navigateFn = vi.fn();
+
+vi.mock('react-router-dom', async () => {
+  const original = await vi.importActual('react-router-dom');
+
+  return { ...original, useNavigate: () => navigateFn };
+});
+
+it('"뒤로 이동" 버튼 클릭시 뒤로 이동하는 navigate(-1) 함수가 호출된다', async () => {
+  const { user } = await render(<ErrorPage />);
+  const button = await screen.getByRole('button', { name: '뒤로 이동' });
+  await user.click(button);
+
+  expect(navigateFn).toHaveBeenNthCalledWith(1, -1);
+});
